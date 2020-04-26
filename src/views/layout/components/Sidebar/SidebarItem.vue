2833a855d563811864ce43b8ae49ae3292b82cb6
Jaydevgit <1187986383@qq.com>
2020-04-26 13:11:56 +0800

    新增 学院科研信息管理 后台管理

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -6,8 +6,11 @@
                    :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
         <el-menu-item :index="item.path+'/'+item.children[0].path" class='submenu-title-noDropdown'>
           <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon"
-                    :icon-class="item.children[0].meta.icon"></svg-icon>
-          <span v-if="item.children[0].meta&&item.children[0].meta.title">{{item.children[0].meta.title}}</span>
+                    :icon-class="item.children[0].meta.icon">
+          </svg-icon>
+          <span v-if="item.children[0].meta&&item.children[0].meta.title">
+            {{item.children[0].meta.title}}
+          </span>
         </el-menu-item>
       </router-link>
 
@@ -31,6 +34,17 @@
       </el-submenu>
 
     </template>
+    <!-- 科研栏目入口（注释即关闭入口）：开始 -->
+    <el-submenu index="1">
+      <template slot="title">
+        <svg-icon icon-class="table"></svg-icon>
+        <span>科研信息管理</span>
+      </template>
+        <el-menu-item index="1-1">正在开发中...</el-menu-item>
+
+
+    </el-submenu>
+    <!-- 科研栏目入口（注释即关闭入口）：结束 -->
   </div>
 </template>
 
