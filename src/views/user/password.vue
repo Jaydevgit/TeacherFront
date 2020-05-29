ce5eb37f0dc255b81e2f25b8b7c35e0f86e4416b
lanshenzz <495102181@qq.com>
2020-05-29 11:06:36 +0800

    超级管理员移交问题

--- src/views/user/password.vue
+++ src/views/user/password.vue
@@ -1,25 +1,49 @@
 <template>
-    <div class="password">
-      <div style="font-family: 'Helvetica Neue';font-size: 20px"><span>修改密码</span></div>
-      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-<!--        <el-form-item label="原始密码" prop="passOld">-->
-<!--          <el-input type="password" v-model="ruleForm.passOld" autocomplete="off"></el-input>-->
-<!--        </el-form-item>-->
-        <el-form-item label="新密码" prop="pass">
-          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码" prop="checkPass">
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-          <el-button @click="resetForm('ruleForm')">清空</el-button>
-        </el-form-item>
-      </el-form>
+  <div>
+      <el-row>
+        <el-col :span="12"><div class="password"> <div style="font-family: 'Helvetica Neue';font-size: 20px;"><span>基本信息</span></div>
+          <el-form :model="ruleForm1" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <!--        <el-form-item label="原始密码" prop="passOld">-->
+            <!--          <el-input type="password" v-model="ruleForm.passOld" autocomplete="off"></el-input>-->
+            <!--        </el-form-item>-->
+            <el-form-item label="姓名" prop="chinese_name">
+              <el-input type="chinese_name" v-model="ruleForm1.chinese_name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="电话" prop="phone">
+              <el-input type="phone" v-model="ruleForm1.phone" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+              <el-input type="email" v-model="ruleForm1.email" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm1()">提交修改</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="password"> <div style="font-family: 'Helvetica Neue';font-size: 20px;"><span>密码修改</span></div>
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+
+              <el-form-item label="新密码" prop="password">
+                <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="确认密码" prop="checkPass">
+                <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                <el-button @click="resetForm('ruleForm')">清空</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
     </div>
 </template>
 
 <script>
+  import {isPhone, isEmail} from '@/utils/validator.js'
     export default {
         name: "password",
       data() {
@@ -58,12 +82,31 @@
             password: '',
             checkPass: '',
           },
+          ruleForm1:{
+            chinese_name: '',
+            phone: '',
+            email: '',
+          },
           rules: {
+            chinese_name: [{required: true, trigger: 'blur', message: "姓名不能为空"}, {
+              max: 30,
+              message: "长度小于30字符"
+            }],
+            phone: [
+              {required: true, trigger: 'blur', message: "电话号码不能为空"},
+              {max: 30, message: "长度小于30字符"},
+              {validator: isPhone, trigger: 'blur'}
+            ],
+            email: [
+              {required: true, trigger: 'blur', message: "邮箱不能为空"},
+              {min: 3, message: "长度大于3字符"},
+              {validator: isEmail, trigger: 'blur'}
+            ],
             // passOld: [
             //   { validator: validatePassOld, trigger: 'blur' }
             // ],
             password: [
-              { validator: validatePass, trigger: 'blur' }
+              { validator: validatePass, trigger: 'blur' },
             ],
             checkPass: [
               { validator: validatePass2, trigger: 'blur' }
@@ -72,9 +115,22 @@
         };
       },
       created(){
+          this.getUserInfo();
          console.log("user="+this.$store.state.user.userId  )
     },
       methods: {
+        getUserInfo(){
+          this.ruleForm.userId=this.$store.state.user.userId
+          this.api({
+            url: "/user/getInfo",
+            method: "post",
+            data: this.ruleForm
+          }).then((res) => {
+            this.ruleForm1.chinese_name=res.nickname;
+            this.ruleForm1.phone=res.phone;
+            this.ruleForm1.email=res.email;
+          })
+        },
         submitForm(formName) {
           this.$refs[formName].validate((valid) => {
             if (valid) {
@@ -97,6 +153,21 @@
             }
           });
         },
+        submitForm1(){
+          this.ruleForm1.userId=this.$store.state.user.userId
+          this.api({
+            url: "/user/updateInfo",
+            method: "post",
+            data: this.ruleForm1
+          }).then((res) => {
+            this.$message({
+              message: "修改成功",
+              type: 'success',
+            })
+          }).catch((e)=>{
+            console.log('error submit!!');
+          })
+        },
         resetForm(formName) {
           this.$refs[formName].resetFields();
         }
@@ -108,7 +179,7 @@
 <style scoped>
   .password{
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-    width: 50%;
+    width: 90%;
     margin: 50px 50px 50px 50px;
     padding: 20px 50px 50px 20px;
   }
