76fa4cfdb77575e88691c1cd0b1eb4e7bd601912
Jaydevgit <1187986383@qq.com>
2020-07-25 19:03:13 +0800

    学校教师信息

--- src/utils/hasSchoolPermission.js
+++ src/utils/hasSchoolPermission.js
@@ -1,10 +1,10 @@
 import store from '../store'
 
 export function hasSchoolPermission(permission) {
-  // console.log("..........我去查下有没有"+permission+"权限")
-  let myPermissions = store.getters.permissions;
-  if(myPermissions.indexOf(permission) > 0) {
-    // console.log("........................找到啦,有"+permission+"权限")
+  console.log("..........我去查下有没有"+permission+"权限")
+  let myPermissions = this.$store.state.schoolUser.permissions;
+  if(myPermissions.indexOf(permission) > -1) {
+    console.log("........................找到啦,有"+permission+"权限")
   }
   return myPermissions.indexOf(permission) > -1;
 }
