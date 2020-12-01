b4f344c094020e942375f5dbbc0c8b71eb7392d3
xiong <1144375027@qq.com>
2020-12-01 18:41:14 +0800

    学校首页菜单栏修改

--- src/views/home/components/LeftNav.vue
+++ src/views/home/components/LeftNav.vue
@@ -6,8 +6,7 @@
           <div class="wp-left-img" v-if="this.unitList[0]">
             <img :src="'http://www.scholat.com/images/uni_logo/'+this.unitList[0].school_name+'.png'"
                  :onerror="defaultLogo"
-                  height="100px"
-                  style="margin-top: 10px"/>
+                 height="100px"/>
           </div>
           <div class="wp-left-content">
             <div v-if="this.unitList[0]">
@@ -25,13 +24,13 @@
         <el-row>
           <el-col>
             <el-menu
-              background-color="#002d54"
+              background-color="#002d5400"
               text-color="#fff"
-              active-text-color="#ffd04b"
+              active-text-color="#fff"
               class="el-menu-vertical-demo"
               :mode="displayMode"
               style="overflow: hidden">
-              <el-menu-item index="0" @click="getAllTeacher()">
+              <el-menu-item style="" active- index="0" @click="getAllTeacher()">
                 <span slot="title" class="parentCatalogue">所有教师</span>
               </el-menu-item>
               <el-menu-item v-for="(item, flag) in unitList" :key="'cId'+item.id" :index="item.id+''" @click="uIdSend(item.id,item.domain_name)">
@@ -51,150 +50,159 @@
 </template>
 
 <script>
-    export default {
-      name: "LeftNav",
-      data(){
-        return{
-          displayMode:'horizontal',
-          unitList:[],
-          listLoading: false,//数据加载等待动画
-          schoolDomain:'',
-          currentCat:'',
-          defaultLogo: 'this.src="http://www.scholat.com/images/uni_logo/nologo.png"',
-        }
-      },
-      created() {
-        this.init();
-      },
-      mounted() {
-
-      },
-      computed: {
-
-      },
-      methods: {
-        init(){
-          this.$store.state.user.schoolDomain=this.$route.path.split('/')[2];
-          this.schoolDomain=this.$route.path.split('/')[2];
-          console.log("schoolDomain="+this.schoolDomain)
-          this.api({
-            url: "/home/"+this.schoolDomain,
-            method: "get",
-          }).then(data => {
-            console.log("查询学校信息为:" + JSON.stringify(data))
-            console.log("================================")
-            this.listLoading = false;
-            this.unitList = data;
-          }).catch(error => {
-            console.log("QAQ........没有找到学校信息")
-          })
-        },
-        getAllTeacher(){
-          this.$router.push(
-            { name:'home',
-              params: {
-                schoolDomain:this.schoolDomain
-          }})
-        },
-        uIdSend: function (uId,unitDomain) {
-          this.currentCat=uId;
-          console.log("send++++" + this.currentCat);
-
-          this.$emit("toList", uId);
-          console.log("leftNav的uId="+uId);
-
-          this.$router.push({
-            path: '/home',
-            name: 'unit',
-            params: {
-              domainName: unitDomain,
-              schoolDomain:this.schoolDomain,
-            }
-          })
-        }
-
-
-      },
+export default {
+  name: "LeftNav",
+  data(){
+    return{
+      displayMode:'horizontal',
+      unitList:[],
+      listLoading: false,//数据加载等待动画
+      schoolDomain:'',
+      currentCat:'',
+      defaultLogo: 'this.src="http://www.scholat.com/images/uni_logo/nologo.png"',
     }
+  },
+  created() {
+    this.init();
+  },
+  mounted() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    init(){
+      this.$store.state.user.schoolDomain=this.$route.path.split('/')[2];
+      this.schoolDomain=this.$route.path.split('/')[2];
+      console.log("schoolDomain="+this.schoolDomain)
+      this.api({
+        url: "/home/"+this.schoolDomain,
+        method: "get",
+      }).then(data => {
+        console.log("查询学校信息为:" + JSON.stringify(data))
+        console.log("================================")
+        this.listLoading = false;
+        this.unitList = data;
+      }).catch(error => {
+        console.log("QAQ........没有找到学校信息")
+      })
+    },
+    getAllTeacher(){
+      this.$router.push(
+        { name:'home',
+          params: {
+            schoolDomain:this.schoolDomain
+          }})
+    },
+    uIdSend: function (uId,unitDomain) {
+      this.currentCat=uId;
+      console.log("send++++" + this.currentCat);
+
+      this.$emit("toList", uId);
+      console.log("leftNav的uId="+uId);
+
+      this.$router.push({
+        path: '/home',
+        name: 'unit',
+        params: {
+          domainName: unitDomain,
+          schoolDomain:this.schoolDomain,
+        }
+      })
+    }
+
+
+  },
+}
 </script>
 
 <style scoped>
-  @import '../../../styles/catalogueVariables.scss';
+@import '../../../styles/catalogueVariables.scss';
 
-  .el-row{
-    /*height: 80px;*/
-    padding: 0;
-    margin-bottom: 0;
-  }
-
-  @media screen and (min-width: 1025px){
-    .parentCatalogue {
-      font-size: 16px;
-      letter-spacing: 3px;
-    }
-
-    .el-menu {
-      background-color:rgba(0,45,84,0.5);
-    }
-
-    .el-menu-item {
-      /*background-color: white;
-      height: 50px;
-      line-height: 50px;*/
-    }
-  }
-  .left-container {
-    /*display: flex;
-    align-items: center;
-    width: 100%;*/
-    position: fixed;
-    top: 0;
-    z-index: 1000;
-    flex-flow: row;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    align-items: center;
-    height: 180px;
-    margin: 0 auto;
-    width: 100%;
-    background-color:rgba(0,45,84,0.8);
-    color: white;
-    padding-top: 5px;
-  }
-  .left-content{
-    max-width: 960px;
-    margin: 0 auto;
-  }
-  .wp{
-    height: 100px;
-    /*border-bottom: 1px solid rgb(0,45,84);*/
-    line-height: 120px;
-
-  }
-  .wp-left{
-    display: flex;
-  }
-  .wp-left-content{
-    display: flex;
-    font-size: 34px;
-    margin-left: 20px;
-    font-family: 楷体;
-  }
-  。wp-left-img{
-
-  }
-  .line{
-    background:#e2e2e2b5;/*背景色为浅灰色*/
-    width:1px;/*设置宽高*/
-    height:40px;
-    margin-left: 10px;
-    margin-right: 10px;
-    margin-top: 40px;
+@media screen and (min-width: 1025px){
+  .parentCatalogue {
+    font-size: 16px;
+    letter-spacing: 3px;
   }
 
-  .ul-nav{
-    transform: translateY(15px);
+  .el-menu {
+    background-color:rgba(0,45,84,0.5);
   }
 
+  .el-menu-item {
+    /*background-color: white;
+    height: 50px;
+    line-height: 50px;*/
+  }
+}
+.left-container {
+  /*display: flex;
+  align-items: center;
+  width: 100%;*/
+  position: absolute;
+  top: 0;
+  flex-flow: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  height: 200px;
+  margin: 0 auto;
+  width: 100%;
+  background-color:rgba(0,45,84,0.5);
+  color: white;
+  padding-top: 5px;
+}
+.left-content{
+  max-width: 960px;
+  margin: 0 auto;
+}
+.wp{
+  height: 120px;
+  /*border-bottom: 1px solid gray;*/
+  line-height: 120px;
 
+}
+.wp-left{
+  display: flex;
+}
+.wp-left-content{
+  display: flex;
+  font-size: 34px;
+  margin-left: 20px;
+  font-family: 楷体;
+}
+.line{
+  background:#E7E7E7;/*背景色为浅灰色*/
+  width:0.6px;/*设置宽高*/
+  height:40px;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 40px;
+}
+
+.ul-nav{
+
+}
+.el-menu--horizontal>.el-menu-item {
+  float: left;
+  height: 60px;
+  line-height: 60px;
+  margin: 0;
+  border-bottom: 0px solid transparent;
+  border-top: 2px solid gray;
+
+}
+.el-menu-item:hover {
+  background-color: #002d5400 !important;
+  text-decoration: underline;
+  border-top: 3px solid #CC9966;
+}
+.is-active {
+  background-color: #002d5400 !important;
+  color: white;
+  text-decoration: underline  ;
+  border-top: 3px solid #CC9966;
+}
 </style>
+
